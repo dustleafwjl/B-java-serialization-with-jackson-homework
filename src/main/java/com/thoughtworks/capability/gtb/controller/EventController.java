85acd7356b06254024488e71189b421452417d7e
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-14 21:11:25 +0800

    [jianglin] feat: finshed problem2

--- src/main/java/com/thoughtworks/capability/gtb/controller/EventController.java
+++ src/main/java/com/thoughtworks/capability/gtb/controller/EventController.java
@@ -6,14 +6,11 @@ import com.thoughtworks.capability.gtb.vo.UserVo;
 import java.time.LocalDateTime;
 import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
+@CrossOrigin
 public class EventController {
 
   @GetMapping("/events/{id}")
